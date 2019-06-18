5fae82e662f1bb150a971fd570b73baa85c77260
estrela530 <daisuke.0530.otk@gmail.com>
2019-06-18 09:52:03 +0900

    ベース

---
+++ teamOPPAL/Assets/Script/EnemyMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float angle;
+    public Vector3 GoalPos;
+    //Random rnd = new Random();
+    List<Vector3> GoalPosList = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GoalPos = new Vector3(Random.Range(95, -95), 0, Random.Range(48, - 48));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        
+    }
+}
---
+++ teamOPPAL/Assets/Script/PlayerBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float shotTime;
+    public GameObject TamaPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Shot"))
+        {
+            Shot();
+            
+        }
+
+    }
+
+    void Shot()
+    {
+        GameObject Tama = Instantiate(TamaPrefab, transform.position, Quaternion.identity);
+        Rigidbody tamarigidbody = Tama.GetComponent<Rigidbody>();
+        tamarigidbody.AddForce(transform.forward * shotTime);
+    }
+}
