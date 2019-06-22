87d34f0e1944db50d6c3d5d7df3f6fb2bb70ea48
estrela530 <daisuke.0530.otk@gmail.com>
2019-06-22 14:05:44 +0900

    反射、Death、地雷設置完成・地雷判定＆Particle途中

--- teamOPPAL/Assets/Script/Death.cs
+++ teamOPPAL/Assets/Script/Death.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Death : MonoBehaviour
 {
     public GameObject TamaPrefab;
+    public int TamaDeadth;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        TamaDeadth = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnCollisionEnter(Collision collision)
@@ -23,6 +24,18 @@ public class Death : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
+            Destroy(TamaPrefab);
+        }
+        else if (collision.gameObject.CompareTag("wall"))
+        {
+            TamaDeadth += 1;
+            if (TamaDeadth == 2)
+            {
+                Destroy(TamaPrefab);
+            }
         }
     }
+
+
+
 }
---
+++ teamOPPAL/Assets/Script/Installation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Installation : MonoBehaviour
+{
+    public float detonationTime;
+    public float radius;
+    public float pawer;
+    public GameObject LandminePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        detonationTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Bomb"))
+        {
+            Detonation();
+        }
+    }
+
+    void Detonation()
+    {
+        GameObject landmine = Instantiate(LandminePrefab, transform.position, Quaternion.identity);
+        detonationTime += Time.deltaTime;
+
+        if (detonationTime >= 5)
+        {
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                if(rb != null)
+                {
+                    rb.AddExplosionForce(pawer, explosionPos, radius, 3.0f);
+                }
+            }
+
+            detonationTime = 0;
+        }
+    }
+}
